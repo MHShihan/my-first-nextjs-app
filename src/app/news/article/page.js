9042743da10f4ea95c9359bd506622d5ccf2e671
MHShihan <mahmodul.no.1@gmail.com>
2024-01-22 21:33:09 +0600

    Added counter component in home page

---
+++ src/app/news/article/page.js
@@ -0,0 +1,9 @@
+const ArticlePage = () => {
+  return (
+    <div>
+      <h1>Welcome to article page</h1>
+    </div>
+  );
+};
+
+export default ArticlePage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
-import React from "react";
+import Counter from "@/components/counter/Counter";
 
-const page = () => {
+const HomePage = () => {
   return (
     <div>
       <h1>My first next js app</h1>
+      <Counter></Counter>
     </div>
   );
 };
 
-export default page;
+export default HomePage;
